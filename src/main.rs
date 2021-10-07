a3078dd28b8d843a7bb10c66d02c4abd1168f348
gipsyh <gipsyh.icu@gmail.com>
2021-10-07 12:30:44 +0800

    enable rand image

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 #[macro_use]
 extern crate rocket;
 use httpdate::HttpDate;
+use rand::prelude::SliceRandom;
 use rocket::http::hyper::header::{IF_MODIFIED_SINCE, LAST_MODIFIED};
 use rocket::http::{ContentType, Header, Status};
 use rocket::request::{self, FromRequest, Outcome};
 use rocket::response::Redirect;
 use rocket::response::Responder;
-use rocket::{uri, Request, Response};
+use rocket::{uri, Request, Response, State};
 use std::ffi::OsStr;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
+
+fn content_type(path: PathBuf) -> Option<ContentType> {
+    path.extension()
+        .and_then(OsStr::to_str)
+        .and_then(ContentType::from_extension)
+}
+
+fn content_type_check(contenttype: &ContentType) -> bool {
+    *contenttype == ContentType::JPEG
+        || *contenttype == ContentType::PNG
+        || *contenttype == ContentType::WEBP
+}
 
 #[derive(Debug)]
 struct IfModifiedSince(Option<HttpDate>);
@@ -57,11 +70,8 @@ impl<'r> Responder<'r, 'static> for Image {
 impl Image {
     async fn new(path: PathBuf, ifmodifiedsince: IfModifiedSince) -> io::Result<Image> {
         let file = File::open(&path)?;
-        let ext = path
-            .extension()
-            .and_then(OsStr::to_str)
-            .ok_or(io::ErrorKind::AddrInUse)?;
-        let contenttype = ContentType::from_extension(ext).unwrap();
+        let contenttype = content_type(path).ok_or(io::ErrorKind::AddrInUse)?;
+        assert!(content_type_check(&contenttype));
         Ok(Self {
             file,
             contenttype,
@@ -71,17 +81,35 @@ impl Image {
 }
 
 #[get("/image/<file>")]
-async fn image<'a>(file: &str, ifmodifiedsince: IfModifiedSince) -> Option<Image> {
+async fn image(file: &str, ifmodifiedsince: IfModifiedSince) -> Option<Image> {
     let path = Path::new("images").join(file);
     Image::new(path, ifmodifiedsince).await.ok()
 }
 
 #[get("/image")]
-fn random_image() -> Redirect {
-    Redirect::to(uri!(image("1.jpeg")))
+fn random_image(state: &State<Vec<String>>) -> Redirect {
+    Redirect::to(uri!(image(
+        state.choose(&mut rand::thread_rng()).unwrap().as_str()
+    )))
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![image, random_image])
+    let mut images: Vec<String> = vec![];
+    for entry in fs::read_dir("images").unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let filename = entry.file_name().into_string().unwrap();
+        let ext = path.extension().and_then(OsStr::to_str);
+        if let Some(ext) = ext {
+            if let Some(ext) = ContentType::from_extension(ext) {
+                if content_type_check(&ext) {
+                    images.push(filename)
+                }
+            }
+        }
+    }
+    rocket::build()
+        .manage(images)
+        .mount("/", routes![image, random_image])
 }
